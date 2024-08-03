9e2be89bc5c351dfabcb6af75c7c4786773efc5b
GizmoGear22 <mpsimmer@gmail.com>
2024-08-03 16:52:02 -0400

    Getting Started

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-	internal class LoginModel
+	public class LoginModel
 	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		public int HashId { get; set; }
 	}
 }
