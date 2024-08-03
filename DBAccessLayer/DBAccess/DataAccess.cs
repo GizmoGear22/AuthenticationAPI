b76b2e2326b2ba7178c65b88753c48d09e15a61f
GizmoGear22 <mpsimmer@gmail.com>
2024-08-03 17:12:39 -0400

    Added has generator and DBAccess EF structure

--- DBAccessLayer/DBAccess/DataAccess.cs
+++ DBAccessLayer/DBAccess/DataAccess.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
 
 namespace DBAccessLayer.DBAccess
 {
-	public class DBAccess
+	public class DBAccess : DbContext
 	{
-
+		public DBAccess(DbContextOptions<DBAccess> options) : base(options) { }
+		public DbSet<LoginModel> LoginCredentials { get; set; }
 	}
 }
