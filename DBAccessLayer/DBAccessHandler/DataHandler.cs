3d84fe0e11b9aeac0cc4d14f6f0f4e12799678d6
GizmoGear22 <mpsimmer@gmail.com>
2024-08-05 16:29:59 -0400

    Added async AddUserToDB

--- DBAccessLayer/DBAccessHandler/DataHandler.cs
+++ DBAccessLayer/DBAccessHandler/DataHandler.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DBAccessLayer.DBAccess;
+using Models;
 
 namespace DBAccessLayer
 {
-	internal class DataHandler
+	public class DataHandler
 	{
+		private readonly DataAccess _dataAccess;
+		public DataHandler(DataAccess dataAccess)
+		{
+			_dataAccess = dataAccess;
+		}
+
+		public async Task<LoginModel> AddNewUserToDBAsync(LoginModel model)
+		{
+			await _dataAccess.AddAsync(model);
+			await _dataAccess.SaveChangesAsync();
+			return model;
+
+		}
 	}
 }
