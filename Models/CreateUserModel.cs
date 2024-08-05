57bd635e22bba86d2b2c011ecaeaa518c1e4880b
GizmoGear22 <mpsimmer@gmail.com>
2024-08-05 15:52:51 -0400

    Added Account Creation Model

---
+++ Models/CreateUserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+	public class CreateUserModel
+	{
+		[Required]
+		public int Id { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string Name { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string Password { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string checkPassword { get; set; }
+	}
+}
