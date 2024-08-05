62f362d5e7ace2832d5f643f2f7e938840de7348
GizmoGear22 <mpsimmer@gmail.com>
2024-08-05 16:04:55 -0400

    Added message delegate and base functions for logic

---
+++ DelegateProcesses/MessageDelegate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateProcesses
+{
+	public class MessageDelegate
+	{
+		public delegate string DelegateMessageDelegate(string message);
+	}
+}
