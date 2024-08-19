1ee00ee57b0f94f83bbc030c55652ea2b74ae9a0
GizmoGear22 <mpsimmer@gmail.com>
2024-08-19 12:38:12 -0400

    new commit

--- APILayer/Program.cs
+++ APILayer/Program.cs
@@ -7,6 +7,8 @@ using LogicLayer.APILogic;
 using Validations.RegexChecker;
 using Validations.UserCreationValidation;
 using Validations;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,7 +30,32 @@ builder.Services.AddTransient<IUserCreationValidationHandler, UserCreationValida
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+	c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Name = "X-Api-Key",
+		Type = SecuritySchemeType.ApiKey,
+		Description = "Input Api Key Here"
+	});
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "ApiKey"
+				}
+			},
+			Array.Empty<string>()
+		}
+	});
+}
+
+);
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(
 	builder => builder.AllowCredentials().AllowAnyHeader().AllowAnyMethod()));
